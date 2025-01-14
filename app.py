7c630cdb29e5ae85c04a5fc055a4254cd0c673ee
unknown <diffelv@gmail.com>
2025-01-13 19:01:53 -0600

    Cambio de titulo explícito

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 car_data = pd.read_csv('vehicles_us.csv')
-st.header('Sitio Web')
+st.header('Sitio Web Venta de autos')
 hist_button = st.button('Construir histograma')
 if hist_button:
      st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
