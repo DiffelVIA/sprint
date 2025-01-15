66ecdd3c6be3d09982dcec5adbe87e532365ceff
unknown <diffelv@gmail.com>
2025-01-15 17:53:29 -0600

    Datos relevantes de consumo

--- app.py
+++ app.py
@@ -14,3 +14,11 @@ if scatter_button:
      st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
      fig = px.scatter(car_data, x="odometer")
      st.plotly_chart(fig, use_container_width=True)
+st.header('Datos agrupados')
+fuel_per_type = car_data.groupby('fuel')['type'].count()
+fig = px.bar(fuel_per_type, title = 'Combustible principal')
+button_1 = st.button('Combustible principal')
+if button_1:
+     st.write('Visualización de combustible principal')
+     fig = px.bar(fuel_per_type, title = 'Combustible principal')
+     st.plotly_chart(fig, use_container_width=True)
